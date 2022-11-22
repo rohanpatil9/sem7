7c09ae15745b599ea7a74af69a20e791fd43c5be
rohanpatil9 <73517883+rohanpatil9@users.noreply.github.com>
2022-11-23 05:17:52 +0530

    Update 3_Bank_account.sol

--- BT/3_Bank_account.sol
+++ BT/3_Bank_account.sol
@@ -1,3 +1,38 @@
+//SMALLER
+
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6;
+contract banking
+{
+    mapping(address=>uint) private user_account;
+    function deposit(uint amount) public payable returns(string memory)
+    {
+        user_account[msg.sender] += amount;
+        return "Deposited Successfully";
+    }
+    function withdraw(uint amount) public payable returns(string memory)
+    {
+        require(user_account[msg.sender]>amount,"Insufficient Balance");
+        require(amount>0,"Amount should be more than zero");
+        user_account[msg.sender]=user_account[msg.sender]-amount;
+        return "Withdrawl Successful";
+    }
+    function user_balance() public view returns(uint)
+    {
+        return user_account[msg.sender];
+    }
+}
+
+
+
+
+
+
+************************************************************************************
+
+BIGGER
+
 pragma solidity >= 0.7.0;
 // Write  a  smart  contract  on  a  test  network,  for  Bank  account  of  a  customer  for
   // following operations: Deposit money | Withdraw Money | Show balance
